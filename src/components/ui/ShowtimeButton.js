963f54aa923d7752a94db8308f9c56c0a0339224
spsekk2127 <spsekk2127@gmail.com>
2025-03-08 18:53:44 +0800

    新增場次按鈕

--- src/components/ui/ShowtimeButton.js
+++ src/components/ui/ShowtimeButton.js
@@ -3,7 +3,10 @@ import BaseButton from './BaseButton';
 const ShowtimeButton = ({ showtime, isSelected, onClick }) => {
   return (
     <BaseButton isSelected={isSelected} onClick={onClick}>
-      {showtime.time}
+      <div className="flex flex-col items-center">
+        <div className="text-sm">{showtime.date}</div>
+        <div className="font-medium">{showtime.startTime}</div>
+      </div>
     </BaseButton>
   );
 };
